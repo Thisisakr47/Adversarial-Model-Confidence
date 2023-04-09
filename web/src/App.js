37736d96812efacb60cc7da09a4a0c4d33c86282
Ayush <ayushk20@iitk.ac.in>
2023-04-09 11:57:23 +0530

    web intial commit

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Shit !!</h1>
+    </>
+  );
+}
+
+// Navbar - model(2), attack(3), dropout %age(3)
+// left panel - bad half, good half
+// right panel - top(3), mid(1), bot(3)
+
+export default App;
